1645f4d8f5c5a6a184da06fe38217c40cb042974
Ben Dunn <benthedunn@gmail.com>
2020-07-06 19:23:47 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.create "Title", 10, "me", "you", true
+  movie = Movie.create
   movie.title = "This is a title."
   __
 end
